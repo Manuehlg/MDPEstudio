91ce726bf892287930536893a2cae0ff9bc3ba38
Manuel Lemus <151822942+Manuehlg@users.noreply.github.com>
2025-09-18 13:15:24 +0200

    Primero de commito

---
+++ src/MDP/repaso/herencia/CuentaDeposito.java
@@ -0,0 +1,9 @@
+package MDP.repaso.herencia;
+
+public class CuentaDeposito extends Cuenta {
+    public CuentaDeposito(double saldo, int historial, Persona persona) {
+        super(saldo, historial, persona);
+
+    }
+
+}
